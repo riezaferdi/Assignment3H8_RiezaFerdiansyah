cf367b583d7b1b6696c3b9e439eee29908ac2570
riezaferdi <riezaferdiansyah@gmail.com>
2024-01-16 11:31:21 +0700

    update .js

--- assignment3.js
+++ assignment3.js
@@ -1,5 +1,6 @@
 //exercies 1
 //check if an array is an arithmethical sequence
+console.log("-------Soal 1-------");
 function isArithmeticProgression(numbers) {
     var diff = numbers[1] - numbers[0];
     for (let i = 1; i < numbers.length; i++) {
@@ -16,7 +17,7 @@ console.log(isArithmeticProgression([2, 4, 6, 8]));
 console.log(isArithmeticProgression([2, 6, 18, 54]));
 console.log(isArithmeticProgression([1, 2, 3, 4, 7, 9]));
 
-console.log("------------------------------------");
+console.log("-------soal 2-------");
 //exercises 2
 //check if an string contain letter a and b which have 3 characters in between
 function threeStepAB(text) {
@@ -33,7 +34,7 @@ console.log(threeStepAB('you are boring'));
 console.log(threeStepAB('barbarian'));
 console.log(threeStepAB('bacon and meat'));
 
-console.log("------------------------------------");
+console.log("-------soal 3-------");
 //exercises 3
 //return many array of 2 number that sum to parameter
 function sumArray(arr, int) {
@@ -51,7 +52,7 @@ function sumArray(arr, int) {
 console.log(sumArray([2, 1, 4, 3], 5));
 console.log(sumArray([1, 8, 10, 3], 11));
 
-console.log("------------------------------------");
+console.log("-------soal 4-------");
 //exercises 4
 //return sub array of array with max sum of numbers
 function arrSum(arr) {
